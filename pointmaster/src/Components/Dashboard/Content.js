5d7dec2e005b5e2a92f523175c15d4c874956863
PavaniKarunarathna <pavanikarunarathna21113@gmail.com>
2024-08-18 11:16:23 +0530

    Added the layout

---
+++ pointmaster/src/Components/Dashboard/Content.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import { Layout } from 'antd';
+
+
+const { Content: AntContent } = Layout;
+
+const Content = () => {
+  return (
+    <div className='content'>
+      
+    </div>
+  )
+}
+
+export default Content;
