fc4adb566b1ce8b6d9ba4db06abb7c7e99a84708
PavaniKarunarathna <pavanikarunarathna21113@gmail.com>
2024-09-19 17:11:28 +0530

    Update Home.js

--- pointmaster/src/Pages/Home/Home.js
+++ pointmaster/src/Pages/Home/Home.js
@@ -1,40 +1,20 @@
-import React, { useState } from 'react';
+import React from 'react';
 import RightContent from './RightContent';
 import LeftContent from './LeftContent';
 import PaymentMethods from './PaymentMethods';
+import { useHomeContext } from '../../Context/HomeContext';
 import './home.css';
 
 function Home() {
-  const [selectedItems, setSelectedItems] = useState([]);
-  const [rightContent, setRightContent] = useState('RightContent');
-  const [paymentInfo, setPaymentInfo] = useState({});
-
-  const handleAddItem = (item) => {
-    setSelectedItems([...selectedItems, item]);
-  };
+  const { rightContent } = useHomeContext(); // Get rightContent from context
 
   return (
     <div className='home-container'>
       <div className='home-left-side'>
-        <LeftContent onAddItem={handleAddItem} />
+        <LeftContent />
       </div>
       <div className='home-right-side'>
-        {rightContent === 'RightContent' ? (
-          <RightContent
-            selectedItems={selectedItems}
-            setSelectedItems={setSelectedItems}
-            setRightContent={setRightContent}
-            setPaymentInfo={setPaymentInfo}
-          />
-        ) : (
-          <PaymentMethods
-            customerDetails={paymentInfo.customerDetails}
-            totalAmount={paymentInfo.totalAmount}
-            totalDiscount={paymentInfo.totalDiscount}
-            setRightContent={setRightContent}
-            selectedItems={selectedItems}
-          />
-        )}
+        {rightContent === 'RightContent' ? <RightContent /> : <PaymentMethods />}
       </div>
     </div>
   );
