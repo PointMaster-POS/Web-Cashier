7dedf1ffb76b7f622fc5452406b619d9e2297ca8
PavaniKarunarathna <pavanikarunarathna21113@gmail.com>
2024-10-19 18:37:45 +0530

    Update HomeContext.js

--- pointmaster/src/Context/HomeContext.js
+++ pointmaster/src/Context/HomeContext.js
@@ -11,6 +11,9 @@ export const HomeProvider = ({ children }) => {
   const [rightContent, setRightContent] = useState('');
   const [isAuthenticated, setIsAuthenticated] = useState(false);
   const [holdBillData, setHoldBillData] = useState(null);
+  const [isQRCodeVisible, setIsQRCodeVisible] = useState(false); 
+  const [searchValue, setSearchValue] = useState('');
+  const [qrCodeScanned, setQrCodeScanned] = useState(false);
 
   const handleAddItem = (item) => {
     const newItem = {
@@ -91,6 +94,7 @@ export const HomeProvider = ({ children }) => {
   const resetCustomerSelection = () => {
     setCustomerDetails({});
     setCustomerSelected(false);
+    
   };
   
 
@@ -152,6 +156,12 @@ export const HomeProvider = ({ children }) => {
         increaseQuantityHold,
         decreaseQuantityHold,
         removeItemHold,
+        isQRCodeVisible,
+        setIsQRCodeVisible,
+        searchValue,
+        setSearchValue,
+        qrCodeScanned,
+        setQrCodeScanned,
       }}
     >
       {children}
